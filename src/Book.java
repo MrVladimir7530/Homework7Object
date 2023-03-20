45185fe99a731e3d96a624bb17e1d76cc1588fbe
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-03-21 00:49:07 +0300

    Second commit

---
+++ src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+    private String name;
+    private Author author;
+    private int yearOfPublication;
+
+    public Book(String name, Author author, int yearOfPublication) {
+        this.name = name;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author=" + author +
+                ", yearOfPublication=" + yearOfPublication +
+                '}';
+    }
+}
